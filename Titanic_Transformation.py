80332ebcd04c54d3e4a1bde60ff9b7cc4c7c1e1f
Bailey <127799539+baileyhsu0631@users.noreply.github.com>
2024-06-14 14:01:07 -0600

    Create Titanic_Transformation.py
    transformation_snowflake_worksheet

---
+++ Titanic_Transformation.py
@@ -0,0 +1,42 @@
+# The Snowpark package is required for Python Worksheets. 
+# You can add more packages by selecting them using the Packages control and then importing them.
+
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.types import IntegerType
+from snowflake.snowpark.functions import udf
+
+def main(session: snowpark.Session): 
+    
+    
+    # Your code goes here, inside the "main" handler.
+    tableName = "TITANIC_ETL.PUBLIC.TITANIC_RAW_DATASET"
+    raw_dataframe = session.table(tableName)
+
+    # Print a sample of the dataframe to standard output.
+    raw_dataframe.show()
+
+    removed_column_data = raw_dataframe.drop(col('Name'), col('Cabin'))
+    print("delete operation")
+
+    removed_column_data = removed_column_data.with_column_renamed(col('Ticket'), 'Ticket Number')
+
+    # try to apply filter condition
+    removed_column_data = removed_column_data.filter((col('AGE')>18)& (col('PCLASS')==1)).select(col('PASSENGERID'), col('SURVIVED'), col('PCLASS'), col('SEX'))
+    
+ 
+
+    removed_column_data.show()
+    # Return value will appear in the Results tab.
+
+    @udf(name="minus_one", is_permanent=True, stage_location="@raw_data_stage2", replace=True)
+    def minus_one(x: int) -> int:
+        return x+3
+
+    @udf(name="map_class", is_permanent=True, stage_location="@raw_data_stage2", replace=True)
+    def map_class(x: int) -> str:
+        class_dic={1:'First Class', 2:'Second Class', 3:'Economy'}
+        return class_dic[x]
+    
+ 
+    return  removed_column_data.withColumn('new', map_class(col('PCLASS')))
